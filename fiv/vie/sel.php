5eec0630167d38cb3144ab7efd69eb6b412dbf2f
botaro <binbotaro3@gmail.com>
2023-05-11 16:19:21 +0900

    2023-05-11-16-19-21

--- fiv/vie/sel.php
+++ fiv/vie/sel.php
@@ -2,7 +2,7 @@
 >
 
 <form>
- <button class="btn btn-warning" name=but value=0>top</button>
+ <button class="btn btn-warning" name=but value=0>fiv</button>
  <button class="btn btn-primary" name=but value=1>1</button>
  <button class="btn btn-info" name=but value=2> 2</button>
  <button class="btn btn-success" name=but value=3> bal</button>
